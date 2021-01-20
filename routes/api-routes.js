e670aa3c251c7bc843d9177628763893a7c2811a
Paul Keldsen <Pkeld148@gmail.com>
2021-01-19 19:59:48 -0500

    added code for creating notes, but doesnt work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,37 @@
 const fs = require("fs");
 const db = require("../db/db.json");
+const path = require("path");
+const uuid = require("uuid");
 
 // API Routes
-module.exports = function(app) {
+module.exports = function (app) {
+  // Route for reading notes
+  app.get("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), function (err, data) {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      res.JSON(notes);
+    });
+  });
 
-app.get("/api/notes", function (req, res) {
-   res.send(db);
-    })
-}
+  // Route for making new notes
+  app.post("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), function (err, data) {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+      newNote.id = uuid.v4();
+      notes.push(newNote);
+
+      const createNote = JSON.stringify(notes);
+      fs.writeFile(
+        path.join(__dirname, "./db/db.json"),
+        createNote,
+        function (err, data) {
+          if (err) throw err;
+        }
+      );
+      res.JSON(newNote);
+    });
+  });
+};
